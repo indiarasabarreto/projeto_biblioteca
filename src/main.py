b5d9a4d8201b33f452b3d35f914d5ce70f6ad6dc
Rodrigo Sá Barreto <rodrigosabarreto@MacBook-Pro-de-Rodrigo.local>
2025-09-07 07:14:07 -0300

    Biblioteca parcialmente implementada

---
+++ src/main.py
@@ -0,0 +1,36 @@
+# Livro
+# - id
+# - titulo
+# - autor
+# - editora
+# - categoria
+
+print("Bem-vindo à biblioteca!")
+
+books = ['O Caibalion', 'O Alquimista']
+
+
+def register_book(title_to_add: str) -> None:
+    books.append(title_to_add)
+    print(f"Livro '{title_to_add}' adicionado à biblioteca.")
+
+
+def main():
+    while (True):
+        print(f'Livros disponíveis: {books}')
+        book_title = input("Digite o título do livro: ")
+        for book in books:
+            if book == book_title:
+                print("Livro encontrado!")
+        else:
+            print("Livro não encontrado!")
+            register_book(book_title)
+        
+   
+
+
+if __name__ == "__main__":
+    print("Iniciando programa...")
+    main()
+    print("Programa finalizado.")
+
