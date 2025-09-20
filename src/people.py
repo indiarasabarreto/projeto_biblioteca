8657fadd80c869379413b11b585d6340727a84d3
indiarasabarreto <indiara86@gmail.com>
2025-09-20 19:40:21 -0300

    nova biblioteca sendo implementada

---
+++ src/people.py
@@ -0,0 +1,20 @@
+people_names = ['Indiara Sá Barreto', 'Rodrigo Sá Barreto', 'Angelo Boreggio', 'Gláuber Brennon'] 
+
+
+
+
+
+
+
+def main ():
+    while(True):
+        print(f'Nomes registrados: {people_names}')
+        registered_names = input('Nomes registrados: ')
+
+
+
+
+if __name__ == "__main__":
+    print("Iniciando programa...")
+    main()
+    print("Programa finalizado.")
