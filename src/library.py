1a0051a79bc362d0cd8ea6b093d3373c1a438238
indiarasabarreto <indiara86@gmail.com>
2025-09-26 04:26:33 -0300

    implementando novas funções à biblioteca

--- src/library.py
+++ src/library.py
@@ -5,33 +5,29 @@
 # - editora
 # - categoria
 
-print("Bem-vindo à biblioteca!")
-
-books = ['O Caibalion', 'O Alquimista']
 
 
-def register_book(title_to_add: str) -> None:
-    books.append(title_to_add)
-    print(f"Livro '{title_to_add}' adicionado à biblioteca.")
+books = []
 
 
-def search_book_title(title_to_search: str) -> bool:
-        for book in books:
-            if book == title_to_search:
-                print("Livro encontrado!")
-                return True
-        print("Livro não encontrado!")
-        return False
-            
+def register_book_by_title(title: str, book: str) -> None:
+    register_book_by_title[book.lower()].append(title)
+    return
+
+# def show_book_by_title(title: str) -> list[str]:
+#     return register_book_by_title[title.lower()]
+
+
+
+
+
 
 def main():
-    while (True):
-        print(f'Livros disponíveis: {books}')
-        book_title = input("Digite o título do livro: ")
-        is_book_found = search_book_title(book_title)
+    print("Bem-vindo à biblioteca!")
 
-        if not is_book_found:
-            register_book(book_title)       
+    registered_book = input('Digite o nome do livro para registrar: ')
+    print(registered_book)
+
    
 
 if __name__ == "__main__":
